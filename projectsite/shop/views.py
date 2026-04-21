fb42c4ee6eb911bd0559c177c79b492863e52926
Juan Miguel Malate <jmalate019@gmail.com>
2026-04-21 23:39:50 +0800

    inital homepage added

--- projectsite/shop/views.py
+++ projectsite/shop/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
+# Create your views here.
 def home(request):
     return HttpResponse("Welcome to Meowy 🐱")
-# Create your views here.
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'shop/home.html', {
+        'products': products
+    })
